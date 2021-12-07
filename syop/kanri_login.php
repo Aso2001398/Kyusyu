3c77201608a47cde0f8464e0f46448d203940a02
rento <2001194@s.asojuku.ac.jp>
2021-12-07 15:05:47 +0900

    shoptuika

---
+++ syop/kanri_login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>管理者ログイン</title>
+    <link rel="stylesheet" href="./css/kanri_rogin.css">
+<body>
+<!---->
+<div class="img">
+<img src="img/logo1.png" alt="" title="九州旅行記" width="300" height="200" >
+</div>
+<div class="login">
+    <h2>管理者ログイン</h2>
+    <p>
+        <input type="email" name="mail"  class="email1" placeholder="メールアドレス">
+    </p>
+    <p>
+        <input type="password" name="password" class="email1" placeholder="パスワード">
+    </p>
+    <br><br>
+    <form action="kanri_top.php" method="post">
+    <p>
+        <button class="btn">ログイン</button>
+    </p>
+    </form>
+</div>
+
+
+
+
+
+</body>
+</html>
