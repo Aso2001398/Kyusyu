caf7cda649f5dd812ce2c69303f21d94bb5d92a4
トウケンカ <Aso2001180>
2021-12-15 15:54:59 +0900

    例（なんか追加します）tou

--- EC/Card.php
+++ EC/Card.php
@@ -1,18 +1,17 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Card</title>
-    <link rel="icon"  type="image/x-icon" sizes="144*144" href="img/favicon.ico">
+    <title>Cart</title>
+    <script src="js/gg.js"></script>
     <link rel="stylesheet" href="css/Cardstyle.css">
-    <link rel="stylesheet" href="../syop/css/backbutton.css">
 </head>
 <body>
 <div class="header">
     <button type="button"  class="original" onclick=history.back()>←戻る</button>
 
     <div class="img">
-        <img src="../syop/img/logo1.png" alt="" title="九州旅行記" width="120" height="60" >
+        <img src="img/logo1.png" alt="" title="九州旅行記" width="120" height="60" >
     </div>
     <div class="search">
         <form action="" method="post">
@@ -32,29 +31,76 @@
     <a href="../kannkouti/hukuoka.php"><p class="spot">観光スポット</p></a>
     <a href="syohin.php"><p class="miyage">お土産一覧</p></a>
 </div>
-<form action="Addresschoose.php" method="post">
-    <div class="info warp">
-        <p><strong>ショッピングカード</strong></p>
-        <hr>
-        <ul>
-            <li class="info_2"> <img src="img/img1.jpg" width="80px"/> </li>
-            <li class="info_3"><a>商品名：ゴマサバ</a></li>
-            <li class="info_4"><a>￥：599</a> </li>
-            <li class="info_6">
-                <a href="javascript:" class="btn btn_plus" role="button" title="增加"></a>
-                <input class="inputNum" value="1" size="1">
-                <a href="javascript:" class="btn btn_minus" role="button" title="减少"></a>
-            </li>
-            <hr>
-            <p class="W1">合計(1個の商品)￥599</p>
-    </div>
-    <div class="into">
-        <p class="W2">合計(1個の商品)
-            <br>
-            <br>:￥599</p>
-
-        <button class="butt">購入に進む</button>
-    </div>
-</form>
+<table>
+    <thead>
+    <tr>
+        <th class="tdone">写真</th>
+        <th class="tdtwo">商品</th>
+        <th class="tdthree">数量</th>
+        <th class="tdfour">単価</th>
+        <th class="tdfive">小計</th>
+        <th class="tdsix">操作</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr class="trclass">
+        <td class="tdone xuhao"><img src="img/img1.jpg" width="80px"/></td>
+        <td class="tdtwo ">ゴマサバ</td>
+        <td class="tdthree"><span class="jiajie"><input type="button" value="-"><span class="num">0</span><input type="button" value="+"></span></td>
+        <td class="tdfour"><span>￥：</span><span class="unit">599</span></td>
+        <td class="tdfive"><span>小計：</span><span class="subtal">0</span></td>
+        <td class="tdsix"><button class="del">削除</button></td>
+    </tr>
+    <tr class="trclass">
+        <td class="tdone xuhao"><img src="img/img2.jpg" width="80px"/></td>
+        <td class="tdtwo">KUMAMOTOマカロンズ</td>
+        <td class="tdthree"><span class="jiajie"><input type="button" value="-"><span class="num">0</span><input type="button" value="+"></span></td>
+        <td class="tdfour"><span>￥：</span><span class="unit">700</span></td>
+        <td class="tdfive"><span>小計：</span><span class="subtal">0</span></td>
+        <td class="tdsix"><button class="del">削除</button></td>
+    </tr>
+    <tr class="trclass">
+        <td class="tdone xuhao"><img src="img/img3.jpg" width="80px"/></td>
+        <td class="tdtwo">のむヨーグルト</td>
+        <td class="tdthree"><span class="jiajie"><input type="button" value="-"><span class="num">0</span><input type="button" value="+"></span></td>
+        <td class="tdfour"><span>￥：</span><span class="unit">500</span></td>
+        <td class="tdfive"><span>小計：</span><span class="subtal">0</span></td>
+        <td class="tdsix"><button class="del">削除</button></td>
+    </tr>
+    <tr>
+        <td class="tdone xuhao"><img src="img/img4.jpg" width="80px"/></td>
+        <td class="tdtwo">阿蘇ミルクチーズプリン</td>
+        <td class="tdthree"><span class="jiajie"><input type="button" value="-"><span class="num">0</span><input type="button" value="+"></span></td>
+        <td class="tdfour"><span>￥：</span><span class="unit">400</span></td>
+        <td class="tdfive"><span>小計：</span><span class="subtal">0</span></td>
+        <td class="tdsix"><button class="del">削除</button></td>
+    </tr>
+    <tr class="trclass">
+        <td class="tdone xuhao"><img src="img/img5.jpg" width="80px"/></td>
+        <td class="tdtwo">果物や野菜のもっちー大福</td>
+        <td class="tdthree"><span class="jiajie"><input type="button" value="-"><span class="num">0</span><input type="button" value="+"></span></td>
+        <td class="tdfour"><span>￥：</span><span class="unit">399</span></td>
+        <td class="tdfive"><span>小計：</span><span class="subtal">0</span></td>
+        <td class="tdsix"><button class="del">削除</button></td>
+    </tr>
+    <tr class="trclass">
+        <td class="tdone xuhao"><img src="img/img6.jpg" width="80px"/></td>
+        <td class="tdtwo">馬刺しソフト燻製</td>
+        <td class="tdthree"><span class="jiajie"><input type="button" value="-"><span class="num">0</span><input type="button" value="+"></span></td>
+        <td class="tdfour"><span>￥：</span><span class="unit">1200</span></td>
+        <td class="tdfive"><span>小計：</span><span class="subtal">0</span></td>
+        <td class="tdsix"><button class="del">削除</button></td>
+    </tr>
+    <tr class="trclass">
+        <td class="tdone xuhao"><img src="img/img7.jpg" width="80px"/></td>
+        <td class="tdtwo">天空のしずく</td>
+        <td class="tdthree"><span class="jiajie"><input type="button" value="-"><span class="num">0</span><input type="button" value="+"></span></td>
+        <td class="tdfour"><span>￥：</span><span class="unit">899</span></td>
+        <td class="tdfive"><span>小計：</span><span class="subtal">0</span></td>
+        <td class="tdsix"><button class="del">削除</button></td>
+    </tr>
+    <tr><td   colspan="6"; class="talast"><span>商品は <span class="goods_num">0</span> 点; 合計金額：￥ <span class="pricetal">0</span></td></tr>
+    </tbody>
+</table>
 </body>
 </html>
